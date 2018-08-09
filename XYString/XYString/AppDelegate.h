8e7892e6945c9d5ea6df6a772da9fa1840af1498
lishibo-iOS <870037485@qq.com>
2018-08-09 17:27:34 +0800

    提交

---
+++ XYString/XYString/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYString
+//
+//  Created by zjjk on 2018/8/8.
+//  Copyright © 2018年 zjjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYString/XYString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYString
+//
+//  Created by zjjk on 2018/8/8.
+//  Copyright © 2018年 zjjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
